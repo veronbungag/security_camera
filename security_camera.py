c7187359c499feb4dad4aa8a8f407ac627ce5759
veronbungag <rveron.bungag@gmail.com>
2024-02-07 23:11:21 +0800

    detecting faces

--- security_camera.py
+++ security_camera.py
@@ -4,11 +4,16 @@ import datetime
 
 cap = cv2.VideoCapture(0)
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascase_frontalface_default.xml")
-body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascase_fullbody_default.xml")
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascase_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascase_fullbody_default.xml")
 
 while True:
     _, frame = cap.read()
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     
     cv2.imshow("Kids Camera", frame)
     
