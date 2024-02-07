fedc90d78f7bc86c43dc26a1b985557b0d596917
veronbungag <rveron.bungag@gmail.com>
2024-02-07 23:16:13 +0800

    drawing faces on video

--- security_camera.py
+++ security_camera.py
@@ -15,6 +15,9 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     
+    for (x, y, width, height) in faces:
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
+
     cv2.imshow("Kids Camera", frame)
     
     if cv2.waitKey(1) == ord('q'):
