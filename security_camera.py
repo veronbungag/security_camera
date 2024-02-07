138f11f147f7b7b3f55920d8a808a883c6082af7
veronbungag <rveron.bungag@gmail.com>
2024-02-07 22:53:02 +0800

    fixing errors and camera is working

--- security_camera.py
+++ security_camera.py
@@ -2,12 +2,14 @@ import cv2
 import time
 import datetime
 
-cap = cv2.VideoCapture(o)
+cap = cv2.VideoCapture(0)
 
 while True:
     _, frame = cap.read()
     
     cv2.imshow("Kids Camera", frame)
     
-    if cv2.waitKey(1) == ord('q')
+    if cv2.waitKey(1) == ord('q'):
         break
+
+ 
